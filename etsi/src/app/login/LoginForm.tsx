bc3dccd36b679c42a5db99477237d9e6c47d6f5f
Prima <widianiprima@gmail.com>
2024-11-21 23:58:38 +0700

    feat: wiring login

--- etsi/src/app/login/LoginForm.tsx
+++ etsi/src/app/login/LoginForm.tsx
@@ -1,39 +1,101 @@
 "use client";
 
+import React, { useState } from "react";
 import styles from "./Login.module.css";
+import { useRouter } from "next/navigation";
+// import Cookies from "js-cookie";
 
 const LoginForm: React.FC = () => {
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [error, setError] = useState("");
+	const [message, setMessage] = useState("");
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
+	const router = useRouter();
+
+	const handleSubmit = async (e: React.FormEvent) => {
+		e.preventDefault();
+
+		setError("");
+		setMessage("");
+
+		const formData = new FormData();
+		formData.append("email", email);
+		formData.append("password", password);
+
+		try {
+			const response = await fetch(
+				`http://localhost:3000/api/login`,
+				{
+					method: "POST",
+                    // headers: { "Content-Type": "multipart/form-data" },
+					body: formData,
+				}
+			);
+
+			const data = await response.json();
+			console.log("Login data:", data);
+            
+			if (response.ok && data.message) {
+				setIsLoggedIn(true);
+				setMessage(data.message || "Login successful!");
+                
+				document.cookie = `token=${data.token}; path=/; max-age=${
+					30 * 24 * 60 * 60
+				}; secure=${process.env.NODE_ENV === "production"}`;
+
+                // Cookies.set('Authorization', `Bearer ${result.accessToken}`, { expires: 7 })
+
+				console.log("Cookie token set:", document.cookie);
+
+				// window.location.href = "/";
+                router.push("/");
+				console.log("Redirection triggered");
+			} else {
+				setError(data.error);
+			}
+		} catch (error) {
+			setError("An error occurred during login.");
+			console.error("Login error:", error);
+		}
+	};
+
 	return (
-		<form className={styles.form} 
-		// onSubmit={handleSubmit}
-		>
+		<form className={styles.form} onSubmit={handleSubmit}>
 			<div className={styles.formGroup}>
-				<label className={styles.label} htmlFor="email">Email Address</label>
+				<label className={styles.label} htmlFor="email">
+					Email Address
+				</label>
 				<input
 					type="email"
 					id="email"
 					name="email"
-					// value={formData.email}
-					// onChange={handleChange}
-					className={styles.input} 
+					value={email}
+					onChange={(e) => setEmail(e.target.value)}
+					className={styles.input}
 					required
 				/>
 			</div>
 			<div className={styles.formGroup}>
-				<label className={styles.label} htmlFor="password">Password</label>
+				<label className={styles.label} htmlFor="password">
+					Password
+				</label>
 				<input
 					type="password"
 					id="password"
 					name="password"
-					// value={formData.password}
-					// onChange={handleChange}
-					className={styles.input} 
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
+					className={styles.input}
 					required
 				/>
 			</div>
 			<button type="submit" className={styles.submitButton}>
-				Register
+				Login
 			</button>
+
+			{error && <div className={styles.error}>{error}</div>}
+			{message && <div className={styles.success}>{message}</div>}
 		</form>
 	);
 };
--- etsi/src/app/login/page.tsx
+++ etsi/src/app/login/page.tsx
@@ -1,20 +1,23 @@
 import LoginForm from "./LoginForm";
 import styles from "./Login.module.css";
 import Image from "next/image";
+import Link from "next/link";
 
 const LoginPage: React.FC = () => {
 	return (
 		<div className={styles.container}>
-			<Image 
-				src="/logo.svg" 
-				alt="Logo" 
-				className={styles.logo} 
-				width={200} 
-				height={100} 
+			<Image
+				src="/logo.svg"
+				alt="Logo"
+				className={styles.logo}
+				width={200}
+				height={100}
 			/>
 			<h1 className={styles.title}>Login</h1>
 			<LoginForm />
-			<h5 className={styles.subsubtitle}>New here? Create account</h5>
+			<h5 className={styles.subsubtitle}>
+				New here? <Link href="/register">Create account</Link>
+			</h5>
 		</div>
 	);
 };
